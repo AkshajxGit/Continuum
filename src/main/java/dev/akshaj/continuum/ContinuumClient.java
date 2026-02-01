cbc887df0d26b9e5cfb3db7b48ea9a5f7bfbe77e
AkshajxGit <akshaj762@gmail.com>
2026-02-01 17:08:38 +0530

    added more things

--- src/main/java/dev/akshaj/continuum/ContinuumClient.java
+++ src/main/java/dev/akshaj/continuum/ContinuumClient.java
@@ -1,5 +1,6 @@
 package dev.akshaj.continuum;
 
+import dev.akshaj.continuum.block.ModBlocks;
 import dev.akshaj.continuum.entity.ModEntities;
 import dev.akshaj.continuum.entity.client.TomahawkProjectileModel;
 import dev.akshaj.continuum.entity.client.TomahawkProjectileRenderer;
@@ -8,9 +9,11 @@ import dev.akshaj.continuum.particle.SulphurFlameParticle;
 import dev.akshaj.continuum.trim.ModTrimMaterials;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.impl.client.rendering.EntityRendererRegistryImpl;
+import net.minecraft.client.render.BlockRenderLayer;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 
@@ -24,5 +27,7 @@ public class ContinuumClient implements ClientModInitializer {
         EntityModelLayerRegistry.registerModelLayer(TomahawkProjectileModel.TOMAHAWK, TomahawkProjectileModel::getTexturedModelData);
         EntityRendererRegistryImpl.register(ModEntities.TOMAHAWK, TomahawkProjectileRenderer::new);
         ParticleFactoryRegistry.getInstance().register(ModParticles.SULPHUR_FLAME_PARTICLE, SulphurFlameParticle.Factory::new);
+        BlockRenderLayerMap.putBlock(ModBlocks.NICKEL_CHAIN, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.NICKEL_BARS, BlockRenderLayer.CUTOUT);
     }
 }
